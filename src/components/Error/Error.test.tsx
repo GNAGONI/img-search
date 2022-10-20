e4c72cf3fcd7e81553ca66a81fb322d30b214931
GNAGONI <nikichetotam@gmail.com>
2022-10-20 20:11:09 +0200

    Implement testing (#4)
    Co-authored-by: mhancharou <mhancharou@scnsoft.com>

--- src/components/Error/Error.test.tsx
+++ src/components/Error/Error.test.tsx
@@ -1,9 +1,21 @@
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import Error from './index';
+import { Text } from '../../constants';
 
-it('renders correctly Error', () => {
-  const tree = renderer
-    .create(<Error />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Error', () => {
+  it('should have snapshot', () => {
+    const tree = renderer
+      .create(<Error />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should behave correctly', () => {
+    render(<Error />);
+    const errorCode = screen.getByText(Text.NOT_FOUND_ERROR_CODE);
+    expect(errorCode).toBeInTheDocument();
+    const errorMessage = screen.getByText(Text.NOT_FOUND_ERROR_MESSAGE);
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,13 +1,43 @@
 import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Form from './index';
 
-it('renders correctly Form', () => {
-  const mockCallback = jest.fn();
-  const tree = renderer
-    .create(
+describe('Form', () => {
+  it('should have snapshot', () => {
+    const mockCallback = jest.fn();
+    const tree = renderer
+      .create(
+        <Form onSubmit={mockCallback}>
+          <></>
+        </Form>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should behave correctly', () => {
+    const mockCallback = jest.fn();
+    const { container } = render(
       <Form onSubmit={mockCallback}>
-        <></>
-      </Form>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+        <input type="text" />
+        <button>Test</button>
+      </Form>
+    );
+
+    const form = container.querySelector('form');
+    expect(form).toBeInTheDocument();
+    expect(form).toBeVisible();
+
+    const button = screen.getByRole('button', { name: 'Test' });
+    expect(button).toBeInTheDocument();
+    expect(button).toBeVisible();
+
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+    expect(input).toBeVisible();
+
+    if (form) {
+      fireEvent.submit(form);
+      expect(mockCallback).toHaveBeenCalledTimes(1);
+    }
+  });
 });
